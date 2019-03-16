1044845346ac361a79bae8a28e31ce2348959e03
Vampires1124 <vampire1124.dev@gmail.com>
2019-03-16 18:00:15 +0800

    two

---
+++ Assets/Script/Test/TeachEqual.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeachEqual : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string tmpA = new string(new char[] { 'H', 'L' });
+        string tmpB = new string(new char[] { 'H', 'L' });
+        Debug.Log(tmpA == tmpB);
+        Debug.Log(tmpA.Equals(tmpB));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
