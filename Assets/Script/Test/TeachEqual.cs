cdc6abe9ea0b818e51dc390ee26b14a448e7b3f1
Vampires1124 <vampire1124.dev@gmail.com>
2019-03-18 18:29:50 +0800

    three

--- Assets/Script/Test/TeachEqual.cs
+++ Assets/Script/Test/TeachEqual.cs
@@ -7,10 +7,25 @@ public class TeachEqual : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        TestParams();
+    }
+
+    void TestEqual()
+    {
         string tmpA = new string(new char[] { 'H', 'L' });
         string tmpB = new string(new char[] { 'H', 'L' });
         Debug.Log(tmpA == tmpB);
         Debug.Log(tmpA.Equals(tmpB));
+    }
+
+    public void Food(int x)
+    {
+        x = 10;
+    }
+
+    void TestParams()
+    {
+
     }
 
     // Update is called once per frame
